c85a7c8639275809543a4009a1a3331821fbdc94
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-06-16 18:26:20 +0600

    checking egypt right triangle is fucking easy except the checking of which one is hypotenuse i mean the highest value, you can easily check it through algorithm built in swap, but when you are checking one by one it made the code little bit longer, thats it

---
+++ CPP-SYNTAX-MASTERY/egypt.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+    
+    int a, b, c;
+    vector<string> output;
+    int count = 0;
+
+    while(cin >> a >> b >> c){
+        if(a==0 && b==0 &&c==0){
+            break;
+        }
+        count++;
+        if(a > b){ swap(a, b);}
+        if(b > c){ swap(b, c);}
+        if(a > b){ swap(a, b);}
+
+        if((((a*a) + (b*b)) == (c * c))){
+            output.push_back("right");
+        }else{
+            output.push_back("wrong");
+        }
+    }
+
+    for(int i = 0; i < count; i++){
+        cout << output[i] << endl;
+    }
+
+    
+    
+
+    return 0;
+}
