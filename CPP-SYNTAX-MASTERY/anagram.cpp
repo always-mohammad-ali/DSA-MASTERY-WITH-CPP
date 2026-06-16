f41240de6a61267aa4d8453752fa1ba15eb4fc63
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-06-16 12:54:20 +0600

    colorful flowers are too fucking disturbing, because it looks easy but, you will be confused with all types of triangles and right-triangles, so be careful

---
+++ CPP-SYNTAX-MASTERY/anagram.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+using namespace std;
+
+int main(){
+    string word1, word2;
+    cin >> word1 >> word2;
+
+    
+   
+
+    
+    return 0;
+}
