38bed5782d37e845bb62cccbcc4061d2639ee3c5
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-06-20 23:52:37 +0600

    comapare string

---
+++ CPP-SYNTAX-MASTERY/string/compareString.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string a = "apple";
+    string b = "apple";
+
+    if(a==b){
+        cout << "same";
+    }else{
+        cout << "different";
+    }
+
+    return 0;
+}
--- CPP-SYNTAX-MASTERY/string/search.cpp
+++ CPP-SYNTAX-MASTERY/string/search.cpp
@@ -7,7 +7,13 @@ int main(){
     string sentence = "I love C++";
     cout << sentence.find("C++") << endl;
 
+    if(sentence.find("java") == string::npos){
+        cout << "not found" << endl;
+    }
+
     cout << sentence.substr(2, 5);
+
+
 
     return 0;
 }
